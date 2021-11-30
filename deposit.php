b5a93435f54329bcc3eadcb06a0a87e7b596c921
JRogers7 <jcr11@email.uscupstate.edu>
2021-11-29 20:26:36 -0500

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -4,13 +4,11 @@
     if (isset($_SESSION["username"])) {
         $loggedOnUser = $_SESSION["username"];
     }
-    echo (isset($_SESSION["username"]));
 
 $depositAmt = $_POST['depositAmt'];
 
 $sql = "SELECT balance from Debit_Card WHERE username = '{$loggedOnUser}'";
 $res = mysqli_query($link, $sql);
-echo $sql;
 
 if (mysqli_num_rows($res) > 0) {
     while ($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
@@ -28,6 +26,6 @@ echo $sql;
 if (mysqli_query($link, $sql)) {
     echo "Balance updated successfully. The balance of your checking account is now $checkingBalance.";
     echo "You will be taken back to the homepage in 5 seconds.";
-    header("refresh:5; location:homepage.php");
+    header("refresh:5; url=homepage.php");
 }
 ?>
