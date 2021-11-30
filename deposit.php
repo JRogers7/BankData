7066a36fc4d8b8b9a4e83e2e6066ec3863b3cc43
JRogers7 <jcr11@email.uscupstate.edu>
2021-11-29 19:38:28 -0500

    implement deposit feature

--- deposit.php
+++ deposit.php
@@ -1,8 +1,32 @@
 <!-- this file will be used with the deposit button to add money to the checking account -->
-<?php require "header.php"; require "config.php" ?>
+<?php require "header.php";
+require "config.php" ?>
 
 <?php
-    $depositAmt = $_POST['depositAmt'];
-    
-    echo $depositAmt + 100;
+if (isset($_SESSION["username"])) {
+    $loggedOnUser = $_SESSION["username"];
+}
+
+$depositAmt = $_POST['depositAmt'];
+
+$sql = "SELECT balance from Debit_Card WHERE username = '{$loggedOnUser}'";
+$res = mysqli_query($link, $sql);
+
+if (mysqli_num_rows($res) > 0) {
+    while ($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+        $checkingBalance = $newArray['balance'];
+    }
+}
+else {
+    $checkingBalance = "N/A";
+}
+
+$checkingBalance += $depositAmt;
+
+$sql = "UPDATE Debit_Card SET balance = '{$checkingBalance}' WHERE username = '{$loggedOnUser}'";
+if (mysqli_query($link, $sql)) {
+    echo "Balance updated successfully. The balance of your checking account is now $checkingBalance.";
+    echo "You will be taken back to the homepage in 5 seconds.";
+    header("refresh:5; location:homepage.php");
+}
 ?>
