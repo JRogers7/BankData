8d57add4159d9163ddc03ed0f1f8f0767267b3a9
JRogers7 <jcr11@email.uscupstate.edu>
2021-12-03 12:16:13 -0500

    Update newcard.php

--- newcard.php
+++ newcard.php
@@ -11,7 +11,12 @@ require "homepage.php"; ?>
     $cvv = rand(pow(10, $cvvDigits-1), pow(10, $cvvDigits)-1);
     $cardNum = rand(pow(10, $cardNumDigits-1), pow(10, $cardNumDigits)-1);
 
+    echo $cvv;
+    echo $cardNum;
+
     $sqlNewCard = "UPDATE Credit_Card SET cvv = '{$cvv}', card_num = '{$cardNum}' WHERE username = '{$loggedOnUser}'";
+
+    echo $sqlNewCard;
     if (mysqli_query($link, $sqlNewCard)) {
         echo "A new card has been issued. Please view your card information again to ensure the card number and CVV have changed.";
     }
